52560c3f8dfb38d01a87dfd576322264de5cb093
Ukhem Fahmi <104829519+UFTHaq@users.noreply.github.com>
2023-11-22 12:19:27 +0800

    Add files via upload

---
+++ P2 - Install & Setup/Proj2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Hello Project 2, My favorite number is 25" << endl;
+    cout << "Enter your favorite number: ";
+    cin >> num;
+    cout << "Your favorite number is " << num << endl;
+    return 0;
+}
