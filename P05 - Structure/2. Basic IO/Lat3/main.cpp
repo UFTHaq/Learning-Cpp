a4f8b8f2c893e62c05fe563084657b1218997496
Ukhem Fahmi <104829519+UFTHaq@users.noreply.github.com>
2023-11-23 12:40:03 +0800

    Add files via upload

---
+++ P05 - Structure/2. Basic IO/Lat3/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int d {};
+    int m {};
+    int y {};
+
+    std::cin >> d >> m >> y;
+
+    std::cout << d << "/" << m << "/" << y << std::endl;
+    return 0;
+}
